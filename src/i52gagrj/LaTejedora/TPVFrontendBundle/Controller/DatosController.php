611f38d715e366131b328e58e31d9370f8f48f16
i52gagrj <i52gagrj@uco.es>
2016-09-02 20:04:27 +0200

    20160902-2004

--- src/i52gagrj/LaTejedora/TPVFrontendBundle/Controller/DatosController.php
+++ src/i52gagrj/LaTejedora/TPVFrontendBundle/Controller/DatosController.php
@@ -246,7 +246,7 @@ class DatosController extends Controller
       if($this->comprobarToken($tokend->id, $tokend->username))
       { 
         $fechahoy = date_format(new \DateTime("now"));
-        if($this->devuelveUltimaFecha()==$fechahoy)
+        if($this->devuelveUltimaFecha()<$fechahoy)
         {  
           // Recuperar el json recibido
           $content = $this->get("request")->getContent();
@@ -385,34 +385,49 @@ class DatosController extends Controller
       //Si los datos del token son correctos, se cargan los productos
       if($this->comprobarToken($tokend->id, $tokend->username))
       { 
-        //Primero buscar la fecha del último cierre
-        //Después pedir las ventas desde esa fecha   
-        $ultimoDiario=$this->devuelveUltimaFecha();    
-        $ventas = $this->devuelveVentas($ultimoDiario);
-        foreach($ventas as $venta)
+        $fechahoy = date_format(new \DateTime("now"));
+        if($this->devuelveUltimaFecha()<$fechahoy)
+        {          
+          //Primero buscar la fecha del último cierre
+          //Después pedir las ventas desde esa fecha   
+          $ultimoDiario=$this->devuelveUltimaFecha();    
+          $ventas = $this->devuelveVentas($ultimoDiario);
+          foreach($ventas as $venta)
+          {
+            if($venta->getContado()) 
+              $contado="Si";
+            else
+              $contado="No";  
+              $elemento = array(
+                'id' => $venta->getId(),
+                'fechaventa' => date_format($venta->getFechaventa(),'Y-m-d'),
+                'horaventa' => date_format($venta->getHoraventa(),'H:i:s'),
+                'socio' => $venta->getSocio()->getNombre(),
+                'contado' => $contado);
+              array_push($respuesta, $elemento);        
+          }    
+          $tokend->iat = time();
+  	      $tokend->exp = time() + 900;
+  	      $jwt = JWT::encode($tokend, '');
+          $mandar = new Response(json_encode(array(
+            'code' => 0,
+            'response'=> array(
+            'token' => $jwt, 
+            'ventas' => $respuesta))));
+          $mandar->headers->set('Content-Type', 'application/json');
+          return $mandar;
+        }
+        else
         {
-          if($venta->getContado()) 
-            $contado="Si";
-          else
-            $contado="No";  
-          $elemento = array(
-            'id' => $venta->getId(),
-            'fechaventa' => date_format($venta->getFechaventa(),'Y-m-d'),
-            'horaventa' => date_format($venta->getHoraventa(),'H:i:s'),
-            'socio' => $venta->getSocio()->getNombre(),
-            'contado' => $contado);
-          array_push($respuesta, $elemento);        
-        }    
-        $tokend->iat = time();
-	      $tokend->exp = time() + 900;
-	      $jwt = JWT::encode($tokend, '');
-        $mandar = new Response(json_encode(array(
-          'code' => 0,
-          'response'=> array(
-          'token' => $jwt, 
-          'ventas' => $respuesta))));
-        $mandar->headers->set('Content-Type', 'application/json');
-        return $mandar;
+          $tokend->iat = time();
+          $tokend->exp = time() + 900;
+          $mandar = new Response(json_encode(array(
+            'code' => 1,
+            'response'=> array(
+              'respuesta' => "El proceso de cierre ya se ha realizado"))));
+          $mandar->headers->set('Content-Type', 'application/json');
+          return $mandar; 
+        }        
       }  
       //Si los datos del token no son correctos, se manda un codigo de error 1 y un mensaje
       else
